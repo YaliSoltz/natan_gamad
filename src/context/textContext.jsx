66918b496ca58463d5e3bf4dc2d0ee7be53d578f
Yali Soltz <yali@sagole.com>
2025-03-11 16:41:10 +0200

    ,

--- src/context/textContext.jsx
+++ src/context/textContext.jsx
@@ -3,15 +3,18 @@ import React, { createContext, useState } from "react";
 export const TextContext = createContext();
 
 const TextProvider = ({ children }) => {
-  const videoUrl = "https://www.youtube.com/embed/yhO3JRf2_o4?autoplay=1";
-  const leftCornerText = ["טקסט", "פינה", "שמאלית"];
+  const videoUrl = "https://www.youtube.com/embed/9OvpF8qsrkE?autoplay=1";
+  const leftCornerText = ["נתן", "הגמד", "שלי"];
   const cardHeader = "כדאי לך ללחוץ על הכרטיס..";
-  const cardTitle = "טקסט כותרת לכרטיס ברכה";
-  const cardSubTitle = "טקסט תחילת ברכה";
+  const cardTitle = "שלום גמד שלי!";
+  const cardSubTitle = "ניחשת כבר מי אני?";
   const cardContent = `
-         טקסט ברכה
+         "אהבה היא הדרך
+         אושר הוא הסימן
+         האור הוא המטרהץ"
+         ד"ר נאדר בוטו 
             `;
-  const blessingFrom = "טקסט סיום ברכה";
+  const blessingFrom = "באהבה, הענק שלך";
   const [isSelectedGift, setSelectedGift] = useState(false);
 
   return (
